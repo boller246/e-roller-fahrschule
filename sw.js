2aba358b6bb1d709419c9bb1d7c7c6c0451dd1fd
boller246 <boller246@gmail.com>
2026-08-25 17:30:56 +0200

    PWA-Cache nach Supabase-Wechsel aktualisieren

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="eroller-v1.2.0";
+const CACHE="eroller-v1.2.1";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./v11.js","./cloud.js","./admin.html","./admin.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
