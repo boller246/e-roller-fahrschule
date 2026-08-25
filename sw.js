a5de74942a26b861e6b3c37ecbb3a0f43043267f
boller246 <boller246@gmail.com>
2026-08-25 23:40:17 +0200

    Bump PWA cache to V1.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="eroller-v1.5.0";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./v11.js","./v12.js","./v13.js","./v14.js","./v15.js","./cloud.js","./admin.html","./admin.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="eroller-v1.6.0";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./v11.js","./v12.js","./v13.js","./v14.js","./v15.js","./v16.js","./reaper.svg","./zeichen350.svg","./cloud.js","./admin.html","./admin.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match("./index.html"))))});
