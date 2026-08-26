739cb20afca1281c60eb80bedfd271adc1a1f36a
boller246 <boller246@gmail.com>
2026-08-26 13:34:15 +0200

    PWA Cache auf V2.1 anheben

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="eroller-v2.0.0";
-const STATIC=["./styles.css?v=20","./app.js?v=20","./v11.js?v=20","./v12.js?v=20","./v13.js?v=20","./v14.js?v=20","./v15.js?v=20","./v16.js?v=20","./v17.js?v=20","./v18.js?v=20","./v19.js?v=20","./v20.js?v=20","./sensenmann2.png?v=20","./b350.png?v=20","./sensenmann.png?v=20","./boller-header.png?v=20","./zeichen350.svg?v=20","./cloud.js?v=20","./admin.html","./admin.js","./manifest.webmanifest?v=20","./icon.svg?v=20"];
+const CACHE="eroller-v2.1.0";
+const STATIC=["./styles.css?v=21","./app.js?v=21","./v11.js?v=21","./v12.js?v=21","./v13.js?v=21","./v14.js?v=21","./v15.js?v=21","./v16.js?v=21","./v17.js?v=21","./v18.js?v=21","./v19.js?v=21","./v20.js?v=21","./v21.js?v=21","./sensenmann2.png?v=21","./b350.png?v=21","./erollerb350.png?v=21","./erollerbier.png?v=21","./erollerbier2.png?v=21","./erollerbier3.png?v=21","./bierfussgaenger.png?v=21","./sensenmannfrei1.png?v=21","./sensenmann.png?v=21","./boller-header.png?v=21","./zeichen350.svg?v=21","./cloud.js?v=21","./admin.html","./admin.js","./manifest.webmanifest?v=21","./icon.svg?v=21"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
