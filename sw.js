8c5990976baf1ba9f7ab727af2f475b898cd31f4
boller246 <boller246@gmail.com>
2026-08-26 09:31:47 +0200

    PWA Cache auf V1.9 anheben

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="eroller-v1.8.0";
-const STATIC=["./styles.css?v=18","./app.js?v=18","./v11.js?v=18","./v12.js?v=18","./v13.js?v=18","./v14.js?v=18","./v15.js?v=18","./v16.js?v=18","./v17.js?v=18","./v18.js?v=18","./reaper.svg?v=18","./zeichen350.svg?v=18","./cloud.js?v=18","./admin.html","./admin.js","./manifest.webmanifest?v=18","./icon.svg?v=18"];
+const CACHE="eroller-v1.9.0";
+const STATIC=["./styles.css?v=19","./app.js?v=19","./v11.js?v=19","./v12.js?v=19","./v13.js?v=19","./v14.js?v=19","./v15.js?v=19","./v16.js?v=19","./v17.js?v=19","./v18.js?v=19","./v19.js?v=19","./sensenmann.png?v=19","./boller-header.png?v=19","./zeichen350.svg?v=19","./cloud.js?v=19","./admin.html","./admin.js","./manifest.webmanifest?v=19","./icon.svg?v=19"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
