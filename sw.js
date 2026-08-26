00968995ea5aac3e0b5b461d5aef607dccd628c5
boller246 <boller246@gmail.com>
2026-08-26 11:07:20 +0200

    PWA Cache auf V2.0 mit sensenmann2 und b350

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="eroller-v1.9.0";
-const STATIC=["./styles.css?v=19","./app.js?v=19","./v11.js?v=19","./v12.js?v=19","./v13.js?v=19","./v14.js?v=19","./v15.js?v=19","./v16.js?v=19","./v17.js?v=19","./v18.js?v=19","./v19.js?v=19","./sensenmann.png?v=19","./boller-header.png?v=19","./zeichen350.svg?v=19","./cloud.js?v=19","./admin.html","./admin.js","./manifest.webmanifest?v=19","./icon.svg?v=19"];
+const CACHE="eroller-v2.0.0";
+const STATIC=["./styles.css?v=20","./app.js?v=20","./v11.js?v=20","./v12.js?v=20","./v13.js?v=20","./v14.js?v=20","./v15.js?v=20","./v16.js?v=20","./v17.js?v=20","./v18.js?v=20","./v19.js?v=20","./v20.js?v=20","./sensenmann2.png?v=20","./b350.png?v=20","./sensenmann.png?v=20","./boller-header.png?v=20","./zeichen350.svg?v=20","./cloud.js?v=20","./admin.html","./admin.js","./manifest.webmanifest?v=20","./icon.svg?v=20"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
